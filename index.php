5b7a07bac2e54257c2a91c733c4beeffbc8be601
Antonio Vinicius Lima Lira <60440970+AntonioVinicius-ff@users.noreply.github.com>
2021-12-27 16:05:41 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google</title>
+</head>
+<body>
+<?php
+function exibirBoasVindas(){
+echo'ola estou apredendo PHP! <br/>';
+}
+exibirBoasVindas();
+function calculandoTerreno($Largura, $comprimento){
+    $area = $Largura * $comprimento;
+    return $area;
+}
+echo calculandoTerreno(5, 25);
+echo '<br/>';
+echo date('d/ m/ Y/ H:i');
+
+echo'<br/>';
+echo date_default_timezone_get();
+?>
+</body>
+</html>
